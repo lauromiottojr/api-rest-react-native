a0907faccd2a8e75913c1fdea51fb200d8237f27
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 11:01:50 -0300

    Creating API NodeJS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import Api from './services/api';
 
 export default class App extends Component {
+
+  signIn = async () => {
+    const response = await api.post('/auth/authenticate', {
+      email: 'lauro@rocketseat.com.br',
+      password: '123456',
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <Button onPress={this.signIn} title='Entrar'></Button>
       </View>
     );
   }
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import { create } from 'apisauce';
+
+const api = create({
+   baseUrl: 'https://old-mule-67.localtunnel.me',
+});
+
+export default api;
